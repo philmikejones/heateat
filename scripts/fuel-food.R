db65baa48b42134da1a28f004a994e73e2523546
Phil Mike Jones <philmikejones+github@gmail.com>
2014-11-03 13:26:17 +0000

    tidied code, map layers in correct order; lsoa/msoa layer code commented out (takes ages to run!)

--- scripts/fuel-food.R
+++ scripts/fuel-food.R
@@ -43,8 +43,7 @@ which(fb$ru == "rural")
 
 fbl <- geom_point(data = fb, aes(OSEAST1M10nov, OSNRTH1M10nov, group = match, 
                                  size = Total, colour = ru))
-ggplot() + llad + fbl + scale_colour_manual(values = c("black", "light grey")) +
-  coord_equal()
+
 
 
 # Voronoi polygon layer ====
@@ -54,60 +53,57 @@ vpm <- geom_segment(data = vp$dirsgs, aes(x = x1, y = y1, xend = x2, yend = y2))
 
 
 # Fuel poverty layer ====
-lihc <- read.csv("data/fplihc.csv", skip = 2, header = T)
-lihc$Estimated.number.of.households <- 
-  as.numeric(lihc$Estimated.number.of.households)
-lihc$Estimated.number.of.Fuel.Poor.Households <- 
-  as.numeric(lihc$Estimated.number.of.Fuel.Poor.Households)
-
-lmlu <- read.csv("data/OA11_LSOA11_MSOA11_LAD11_EW_LUv2.csv", header = T)
-lmlu <- subset(lmlu, select = c("LSOA11CD", "MSOA11CD"))
-lmlu <- unique(lmlu)
-
-lihc <- merge(lihc, lmlu, by.x = "LSOA.Code", by.y = "LSOA11CD")
-rm(lmlu)
-
-msoas <- unique(lihc$MSOA11CD)
-lihc$Estimated.number.of.Fuel.Poor.Households <- 
-  as.numeric(lihc$Estimated.number.of.Fuel.Poor.Households)
-lihc$fphh <- NA
-for(i in 1:NROW(msoas)){
-  lihc$fphh[lihc$MSOA11CD == msoas[i]] <-
-    sum(lihc$Estimated.number.of.Fuel.Poor.Households[lihc$MSOA11CD == msoas[i]])
-}
-lihc <- subset(lihc, select = c("MSOA11CD", "fphh"))
-lihc <- unique(lihc)
-rm(i, msoas)
-
-msoahh <- read.csv("data/eng-msoa-households.csv")
-msoahh <- subset(msoahh, select = c("geography.code", 
-                                    "Tenure..All.households..measures..Value"))
-lihc   <- merge(lihc, msoahh, by.x = "MSOA11CD", by.y = "geography.code")
-lihc$pfphh <- NA
-lihc$pfphh <- (lihc$fphh / lihc$Tenure..All.households..measures..Value) * 100
-
-urmsoa <- read.csv("data/RUC11_MSOA11_EW.csv", header = T)
-lihc   <- merge(lihc, urmsoa, by = "MSOA11CD")
-rm(urmsoa, msoahh)
-
-lihc <- subset(lihc, pfphh >= quantile(pfphh, 0.8))
-
-emsoa <- readOGR(dsn = "../../Boundary Data/MSOAs/England", "England_msoa_2011")
-emsoaf <- fortify(emsoa, region = "CODE")
-emsoaf <- merge(emsoaf, emsoa@data, by.x = "id", by.y = "CODE")
-emsoaf <- merge(emsoaf, lihc, by.x = "id", by.y = "MSOA11CD")
-rm(emsoa, lihc)
-fq <- geom_polygon(data = emsoaf, aes(long, lat, group = group), 
-                                      fill = "red")
-
-
-
-# Final map ====
-ggplot() + llad + fbloc + vpm + fq + coord_equal()
+# MSOA quintile ====
+# lihc <- read.csv("data/fplihc.csv", skip = 2, header = T)
+# lihc$Estimated.number.of.households <- 
+#   as.numeric(lihc$Estimated.number.of.households)
+# lihc$Estimated.number.of.Fuel.Poor.Households <- 
+#   as.numeric(lihc$Estimated.number.of.Fuel.Poor.Households)
+# 
+# lmlu <- read.csv("data/OA11_LSOA11_MSOA11_LAD11_EW_LUv2.csv", header = T)
+# lmlu <- subset(lmlu, select = c("LSOA11CD", "MSOA11CD"))
+# lmlu <- unique(lmlu)
+# 
+# lihc <- merge(lihc, lmlu, by.x = "LSOA.Code", by.y = "LSOA11CD")
+# rm(lmlu)
+# 
+# msoas <- unique(lihc$MSOA11CD)
+# lihc$Estimated.number.of.Fuel.Poor.Households <- 
+#   as.numeric(lihc$Estimated.number.of.Fuel.Poor.Households)
+# lihc$fphh <- NA
+# for(i in 1:NROW(msoas)){
+#   lihc$fphh[lihc$MSOA11CD == msoas[i]] <-
+#     sum(lihc$Estimated.number.of.Fuel.Poor.Households[lihc$MSOA11CD == msoas[i]])
+# }
+# lihc <- subset(lihc, select = c("MSOA11CD", "fphh"))
+# lihc <- unique(lihc)
+# rm(i, msoas)
+# 
+# msoahh <- read.csv("data/eng-msoa-households.csv")
+# msoahh <- subset(msoahh, select = c("geography.code", 
+#                                     "Tenure..All.households..measures..Value"))
+# lihc   <- merge(lihc, msoahh, by.x = "MSOA11CD", by.y = "geography.code")
+# lihc$pfphh <- NA
+# lihc$pfphh <- (lihc$fphh / lihc$Tenure..All.households..measures..Value) * 100
+# 
+# urmsoa <- read.csv("data/RUC11_MSOA11_EW.csv", header = T)
+# lihc   <- merge(lihc, urmsoa, by = "MSOA11CD")
+# rm(urmsoa, msoahh)
+# 
+# lihc <- subset(lihc, pfphh >= quantile(pfphh, 0.8))
+# 
+# emsoa <- readOGR(dsn = "../../Boundary Data/MSOAs/England", "England_msoa_2011")
+# emsoaf <- fortify(emsoa, region = "CODE")
+# emsoaf <- merge(emsoaf, emsoa@data, by.x = "id", by.y = "CODE")
+# emsoaf <- merge(emsoaf, lihc, by.x = "id", by.y = "MSOA11CD")
+# rm(emsoa, lihc)
+# qmsoa <- geom_polygon(data = emsoaf, aes(long, lat, group = group), 
+#                                       fill = "red")
 
 
 
-# # Fuel poverty priority LSOAs ====
+# Priority LSOAs ====
+# commented out because it takes bloody ages
 # lsoa <- readOGR(dsn = "../../Boundary Data/LSOAs/eng-lsoa-2011", 
 #                 "england_lsoa_2011Polygon")
 # proj4string(lsoa) <- CRS("+init=epsg:27700")
@@ -119,5 +115,13 @@ ggplot() + llad + fbloc + vpm + fq + coord_equal()
 # lsoaf         <- fortify(lsoa, region = "code")
 # lsoaf         <- merge(lsoaf, lsoa@data, by.x = "id", by.y = "code")
 # 
-# f <- ggplot(lsoaf, aes(long, lat, group = group, fill = priority))
-# f + geom_polygon() + coord_equal()
+# plsoa <- geom_polygon(data = lsoaf, aes(long, lat, group = group, 
+# fill = priority))
+
+
+
+# Final map ====
+ggplot() + llad + fbl + scale_colour_manual(values = c("black", "light grey")) +
+  vpm + coord_equal()
+# + plsoa
+# + qmsoa
