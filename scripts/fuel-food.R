8016b482e51026ad04fed73b96232a20568945d9
Phil Mike Jones <philmikejones+github@gmail.com>
2014-12-03 11:45:10 +0000

    imported lad/region lookup

--- scripts/fuel-food.R
+++ scripts/fuel-food.R
@@ -48,6 +48,11 @@ elad <- readOGR(dsn = "shapes/englandLADs/",
                 "england_lad_2011Polygon")
 proj4string(elad) <- CRS("+init=epsg:27700")
 
+# region/LAD lookup from https://geoportal.statistics.gov.uk/geoportal/catalog/search/resource/details.page?uuid={B6A77A55-DB44-4C3C-901F-82742CB6A54B}
+unzip(zipfile = "data/Countries_(2012)_to_government_office_regions_(2010)_to_counties_(2012)_to_local_authority_districts_(2012)_to_wards_(2012)_UK_lookup.zip",
+      files = "CTRY12_GOR10_CTY12_LAD12_WD12_UK_LU.csv", exdir = "data/")
+rllook <- read.csv("data/CTRY12_GOR10_CTY12_LAD12_WD12_UK_LU.csv")
+
 
 
 # Fuel poverty layer ====
@@ -77,8 +82,13 @@ yhr  <- ereg[ereg$CODE == "E12000003", ]
 yhrf <- fortify(yhr, region = "CODE")
 yhrf <- merge(yhrf, yhr, by.x = "id", by.y = "CODE")
 yhl  <- gIntersection(yhr, yhl, byid = T, drop_not_poly = T)
+yhl  <- SpatialPolygonsDataFrame(yhl, elad)
+yhlf <- fortify(yhl, region = "")
+head(elad@data)
 
 ggplot() +
+  geom_polygon(data = yhlf, aes(x = long, y = lat, group = group),
+               fill = "transparent", colour = "dark grey") +
   geom_polygon(data = yhrf, aes(x = long, y = lat, group = group),
                fill = "transparent", colour = "light grey") +
   map + coord_equal()
