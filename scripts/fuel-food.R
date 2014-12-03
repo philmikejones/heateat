3fc5976b680d8ba191bee262976cf34fc511c374
Phil Mike Jones <philmikejones+github@gmail.com>
2014-12-03 16:48:47 +0000

    working on lad clip to region

--- scripts/fuel-food.R
+++ scripts/fuel-food.R
@@ -32,6 +32,17 @@ land <- c(42/2.54, 29.7/2.54)
 ereg <- readOGR(dsn = "shapes/ewregions/",
                "England_gor_2011")
 proj4string(ereg) <- CRS("+init=epsg:27700")
+
+eer <- ereg[ereg$CODE == "E12000006", ]
+lr  <- ereg[ereg$CODE == "E12000007", ]
+nwr <- ereg[ereg$CODE == "E12000002", ]
+ner <- ereg[ereg$CODE == "E12000001", ]
+emr <- ereg[ereg$CODE == "E12000004", ]
+yhr <- ereg[ereg$CODE == "E12000003", ]
+swr <- ereg[ereg$CODE == "E12000009", ]
+wmr <- ereg[ereg$CODE == "E12000005", ]
+ser <- ereg[ereg$CODE == "E12000008", ]
+
 #   CODE                          NAME
 # 0 E12000006          East of England
 # 1 E12000007                   London
@@ -47,20 +58,7 @@ proj4string(ereg) <- CRS("+init=epsg:27700")
 elad <- readOGR(dsn = "shapes/englandLADs/", 
                 "england_lad_2011Polygon")
 proj4string(elad) <- CRS("+init=epsg:27700")
-
-# region/LAD lookup from https://geoportal.statistics.gov.uk/geoportal/catalog/search/resource/details.page?uuid={B6A77A55-DB44-4C3C-901F-82742CB6A54B}
-# unzip(zipfile = "data/Countries_(2012)_to_government_office_regions_(2010)_to_counties_(2012)_to_local_authority_districts_(2012)_to_wards_(2012)_UK_lookup.zip",
-#       files = "CTRY12_GOR10_CTY12_LAD12_WD12_UK_LU.csv", exdir = "data/")
-rllu <- read.csv("data/CTRY12_GOR10_CTY12_LAD12_WD12_UK_LU.csv", header = T)
-rllu <- rllu[, c(4, 6, 10, 12)]
-rllu <- rllu[grep("E", rllu$LAD12CD), ]
-rllu <- unique(rllu)
-elad$code <- as.character(elad$code)
-elad$code[elad$code == "E07000100"] <- "E07000240"
-elad$code[elad$code == "E07000104"] <- "E07000241"
-elad@data <- merge(elad@data, rllu, by.x = "code", by.y = "LAD12CD")
-
-
+gIntersection(elad, eer, byid = T)
 
 # Fuel poverty layer ====
 # LSOAs
@@ -70,32 +68,4 @@ proj4string(elsoa) <- CRS("+init=epsg:27700")
 
 
 
-# # Food bank layer ====
-# fbt <- read.csv("data/foodbanks.csv")
-# fbm <- read.csv("data/foodbanks-matched.csv")
-# fb  <- merge(fbt, fbm, by = "match")
-# rm(fbt, fbm)
-# fb <- fb[fb$URINDEW10nov != 9, ]  # 9 is Scotland/NI/Channel Is/IoM, see docs
-# fb$Total[fb$Total == -99] <- NA
-# # create projection for clipping
-# coordinates(fb) <- c("OSEAST1M10nov", "OSNRTH1M10nov")
-# proj4string(fb) <- CRS("+init=epsg:27700")
-# fb <- spTransform(fb, CRSobj = CRS(proj4string(reg)))
-
-
-
 # # Final maps ====
-yhr  <- ereg[ereg$CODE == "E12000003", ]
-yhrf <- fortify(yhr, region = "CODE")
-yhrf <- merge(yhrf, yhr, by.x = "id", by.y = "CODE")
-yhl  <- gIntersection(yhr, yhl, byid = T, drop_not_poly = T)
-yhl  <- SpatialPolygonsDataFrame(yhl, elad)
-yhlf <- fortify(yhl, region = "")
-head(elad@data)
-
-ggplot() +
-  geom_polygon(data = yhlf, aes(x = long, y = lat, group = group),
-               fill = "transparent", colour = "dark grey") +
-  geom_polygon(data = yhrf, aes(x = long, y = lat, group = group),
-               fill = "transparent", colour = "light grey") +
-  map + coord_equal()
