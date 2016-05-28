123d4ea71b8a55afaff401a8a2f6fbe3c4274ead
Phil Mike Jones <philmikejones@gmail.com>
2016-05-28 20:26:14 +0100

    build package

--- R/prep-shapefiles.R
+++ R/prep-shapefiles.R
@@ -1,7 +1,6 @@
 # Set up cores
 cores <- parallel::detectCores()
 
-
 # Set up the directories necessary for download.file()
 setup_dir("data/")
 setup_dir("data/shapes/")
@@ -15,7 +14,7 @@ if (!file.exists("data/shapes/regions.zip")) {
   message("Fetching shapefile(s)...")
   download.file(
     "https://census.edina.ac.uk/ukborders/easy_download/prebuilt/shape/England_gor_2011_gen.zip",
-    method = "wget", destfile = "data/shapes/regions.zip"
+    destfile = "data/shapes/regions.zip"
   )
 }
 
@@ -33,7 +32,7 @@ if (!file.exists("data/shapes/lsoas.zip")) {
   message("Fetching shapefile(s)...")
   download.file(
     "https://census.edina.ac.uk/ukborders/easy_download/prebuilt/shape/England_lsoa_2011_gen.zip",
-    method = "wget", destfile = "data/shapes/lsoas.zip"
+    destfile = "data/shapes/lsoas.zip"
   )
 }
 
@@ -51,88 +50,69 @@ if (!file.exists("data/shapes/lsoas/england_lsoa_2011_gen.shp")) {
 }
 
 
-# Load regions
-regions <- rgdal::readOGR(dsn = "data/shapes/regions", "england_gor_2011_gen")
-
-# Filter each region
-regions_list <- list()
-for (i in 1:nrow(regions@data)) {
-  regions_list[[i]] <- regions[i, ]
-}
-
-
-# Clip LADs
-lads <- rgdal::readOGR(dsn = "data/shapes/lads", "england_lad_2011_gen")
-lads_list <- list()
-
-# Subset first; it's more resource-efficient than gIntersection()
-lad_east_e <- lads[reg_east_e, ]
-lad_london <- lads[reg_london, ]
-lad_n_west <- lads[reg_n_west, ]
-lad_n_east <- lads[reg_n_east, ]
-lad_east_m <- lads[reg_east_m, ]
-lad_york_h <- lads[reg_york_h, ]
-lad_s_west <- lads[reg_s_west, ]
-lad_west_m <- lads[reg_west_m, ]
-lad_s_east <- lads[reg_s_east, ]
-
-lad_east_e <- rgeos::gIntersection(lad_east_e, reg_east_e, byid = TRUE)
-lad_london <- rgeos::gIntersection(lad_london, reg_london, byid = TRUE)
-lad_n_west <- rgeos::gIntersection(lad_n_west, reg_n_west, byid = TRUE)
-lad_n_east <- rgeos::gIntersection(lad_n_east, reg_n_east, byid = TRUE)
-lad_east_m <- rgeos::gIntersection(lad_east_m, reg_east_m, byid = TRUE)
-lad_york_h <- rgeos::gIntersection(lad_york_h, reg_york_h, byid = TRUE)
-lad_s_west <- rgeos::gIntersection(lad_s_west, reg_s_west, byid = TRUE)
-lad_west_m <- rgeos::gIntersection(lad_west_m, reg_west_m, byid = TRUE)
-lad_s_east <- rgeos::gIntersection(lad_s_east, reg_s_east, byid = TRUE)
-
-
-
-regions_lads <- parallel::mclapply(regions_lads, function(x)
-  rgeos::gIntersection(x, spgeom2 = regs, byid = TRUE)
-)
-
-
-# LSOAs
-lsoa <- rgdal::readOGR(dsn = "data/shapes/lsoas", "england_lsoa_2011_gen")
-
-# Split LSOAs into regions
-# Subset first because it's efficient
-regions_lsoa <- list(
-  easte_lsoa = lsoa[regions[["easte"]], ],
-  lond_lsoa  = lsoa[regions[["lond"]], ],
-  nortw_lsoa = lsoa[regions[["nortw"]], ],
-  norte_lsoa = lsoa[regions[["norte"]], ],
-  eastm_lsoa = lsoa[regions[["eastm"]], ],
-  yorks_lsoa = lsoa[regions[["yorks"]], ],
-  soutw_lsoa = lsoa[regions[["soutw"]], ],
-  westm_lsoa = lsoa[regions[["westm"]], ],
-  soute_lsoa = lsoa[regions[["soute"]], ]
-)
-regions_lsoa <- parallel::mclapply(regions_lsoa, function(x)
-  rgeos::gIntersection(x, spgeom2 = regs, byid = TRUE)
-)
-
-
-
-# Load fuel poverty data
-setup_dir("data/raw/")
-if (!file.exists("data/raw/fuel-poverty.xlsx")) {
-  message("Obtaining fuel poverty data...")
-  download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/485161/2013_Sub-regional_tables.xlsx",
-                destfile = "data/raw/fuel-poverty.xlsx")
-}
-fp <- readxl::read_excel("data/raw/fuel-poverty.xlsx",
-                         sheet = "Table 3", skip = 1, col_names = TRUE)
-fp <- dplyr::select(fp, 1, 6, 7, 8)
-colnames(fp) <- c("code", "num_hh", "num_fph", "per_fph")
-fp <- na.omit(fp)
-
-# Join to shapefile
-lsoa@data$code <- as.character(lsoa@data$code)
-lsoa@data <- dplyr::inner_join(lsoa@data, fp, by = "code")
-
-
+# # Load regions
+# regions <- rgdal::readOGR(dsn = "data/shapes/regions", "england_gor_2011_gen")
+#
+# # Separate each region
+# regions_codes <- as.list(as.character(unique(regions@data$name)))
+#
+# regions_list <- list()
+# for (i in seq_along(regions_codes)) {
+#   regions_list[[i]] <- regions[regions@data$name == regions_codes[[i]], ]
+# }
+# rm(i)
+#
+#
+# # Load LADs
+# lads <- rgdal::readOGR(dsn = "data/shapes/lads", "england_lad_2011_gen")
+#
+# # Clip LADs to each region
+# lads_list <- list()
+# for (i in seq_along(1:length(regions_codes))) {
+#   lads_list[[i]] <- lads[regions_list[[i]], ]
+# }
+#
+#
+# # LSOAs
+# lsoa <- rgdal::readOGR(dsn = "data/shapes/lsoas", "england_lsoa_2011_gen")
+#
+# # Split LSOAs into regions
+# # Subset first because it's efficient
+# regions_lsoa <- list(
+#   easte_lsoa = lsoa[regions[["easte"]], ],
+#   lond_lsoa  = lsoa[regions[["lond"]], ],
+#   nortw_lsoa = lsoa[regions[["nortw"]], ],
+#   norte_lsoa = lsoa[regions[["norte"]], ],
+#   eastm_lsoa = lsoa[regions[["eastm"]], ],
+#   yorks_lsoa = lsoa[regions[["yorks"]], ],
+#   soutw_lsoa = lsoa[regions[["soutw"]], ],
+#   westm_lsoa = lsoa[regions[["westm"]], ],
+#   soute_lsoa = lsoa[regions[["soute"]], ]
+# )
+# regions_lsoa <- parallel::mclapply(regions_lsoa, function(x)
+#   rgeos::gIntersection(x, spgeom2 = regs, byid = TRUE)
+# )
+#
+#
+#
+# # Load fuel poverty data
+# setup_dir("data/raw/")
+# if (!file.exists("data/raw/fuel-poverty.xlsx")) {
+#   message("Obtaining fuel poverty data...")
+#   download.file("https://www.gov.uk/government/uploads/system/uploads/attachment_data/file/485161/2013_Sub-regional_tables.xlsx",
+#                 destfile = "data/raw/fuel-poverty.xlsx")
+# }
+# fp <- readxl::read_excel("data/raw/fuel-poverty.xlsx",
+#                          sheet = "Table 3", skip = 1, col_names = TRUE)
+# fp <- dplyr::select(fp, 1, 6, 7, 8)
+# colnames(fp) <- c("code", "num_hh", "num_fph", "per_fph")
+# fp <- na.omit(fp)
+#
+# # Join to shapefile
+# lsoa@data$code <- as.character(lsoa@data$code)
+# lsoa@data <- dplyr::inner_join(lsoa@data, fp, by = "code")
+#
+#
 # proj4string(regs) <- CRS("+init=epsg:27700")
 # proj4string(lads) <- CRS("+init=epsg:27700")
 # proj4string(lsoa) <- CRS("+init=epsg:27700")
