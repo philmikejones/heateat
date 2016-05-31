ffd9e87c988829a4459797e18d414b25d6d51e2f
Phil Mike Jones <philmikejones@gmail.com>
2016-05-31 16:45:57 +0100

    use non-generalised shapefiles

--- R/prep-shapefiles.R
+++ R/prep-shapefiles.R
@@ -12,7 +12,7 @@ dir.create("data/raw/",         recursive = TRUE, showWarnings = FALSE)
 if (!file.exists("data/shapes/lads.zip")) {
   message("Fetching shapefile(s)...")
   download.file(
-    "https://census.edina.ac.uk/ukborders/easy_download/prebuilt/shape/England_lad_2011_gen.zip",
+    "https://census.edina.ac.uk/ukborders/easy_download/prebuilt/shape/England_lad_2011.zip",
     mode = "wb", method = "wget", destfile = "data/shapes/lads.zip"
   )
 }
@@ -21,7 +21,7 @@ if (!file.exists("data/shapes/lads.zip")) {
 if (!file.exists("data/shapes/lsoas.zip")) {
   message("Fetching shapefile(s)...")
   download.file(
-    "https://census.edina.ac.uk/ukborders/easy_download/prebuilt/shape/England_lsoa_2011_gen.zip",
+    "https://census.edina.ac.uk/ukborders/easy_download/prebuilt/shape/England_lsoa_2011.zip",
     destfile = "data/shapes/lsoas.zip", method = "wget", mode = "wb"
   )
 }
@@ -37,7 +37,7 @@ if (!file.exists("data/shapes/lsoas/england_lsoa_2011_gen.shp")) {
 
 
 # Load LADs
-lads <- rgdal::readOGR(dsn = "data/shapes/lads", "england_lad_2011_gen")
+lads <- rgdal::readOGR(dsn = "data/shapes/lads", "england_lad_2011")
 lads@data$label <- as.character(lads@data$label)
 
 # Filter South Yorkshire
@@ -47,12 +47,21 @@ lads <- lads[lads@data$name == "Barnsley"  |
              lads@data$name == "Sheffield", ]
 
 # LSOAs
-lsoa <- rgdal::readOGR(dsn = "data/shapes/lsoa", "england_lsoa_2011_gen")
+lsoa <- rgdal::readOGR(dsn = "data/shapes/lsoa", "england_lsoa_2011")
 lsoa@data$code <- as.character(lsoa@data$code)
 lsoa_data <- lsoa@data
+
+lsoa <- lsoa[lads, ]
 lsoa <- rgeos::gIntersection(lads, lsoa, byid = TRUE, drop_lower_td = TRUE)
-stop()
-sp::spChFIDs()
+
+row.names(lsoa) <- gsub("9 ",   "", row.names(lsoa))
+row.names(lsoa) <- gsub("214 ", "", row.names(lsoa))
+row.names(lsoa) <- gsub("297 ", "", row.names(lsoa))
+row.names(lsoa) <- gsub("320 ", "", row.names(lsoa))
+
+lsoa <- sp::SpatialPolygonsDataFrame(lsoa,
+  lsoa_data[row.names(lsoa_data) %in% row.names(lsoa), ])
+
 
 # Load fuel poverty data
 if (!file.exists("data/raw/fuel-poverty.xlsx")) {
@@ -67,23 +76,22 @@ fp <- dplyr::select(fp, 1, 6, 7, 8)
 fp <- na.omit(fp)
 colnames(fp) <- c("code", "num_hh", "num_fph", "per_fph")
 
-if (!(nrow(fp) == nrow(lsoa@data))) {
+if (!(nrow(fp) == nrow(lsoa_data))) {
   warning("LSOA row numbers do not match")
   stop()
 }
+rm(lsoa_data)
 
-# # Join to shapefile
-# lsoa@data <- dplyr::inner_join(lsoa@data, fp, by = "code")
-#
-# # Fortify shapefiles
-# regs_f <- ggplot2::fortify(regs, region = "label")
-# regs_f <- dplyr::inner_join(regs_f, regs@data, by = c("id" = "label"))
-# save(regs_f, file = "data/regions.RData")
-#
-# lads_f <- ggplot2::fortify(lads, region = "label")
-# lads_f <- dplyr::inner_join(lads_f, lads@data, by = c("id" = "label"))
-# save(lads_f, file = "data/lads.RData")
-#
-# lsoa_f <- ggplot2::fortify(lsoa, region = "code")
-# lsoa_f <- dplyr::inner_join(lsoa_f, lsoa@data, by = c("id" = "code"))
-# save(lsoa_f, file = "data/lsoa.RData")
+# Join to shapefile
+lsoa@data <- dplyr::inner_join(lsoa@data, fp, by = "code")
+
+# Fortify shapefiles
+lads_f <- ggplot2::fortify(lads, region = "label")
+lads_f <- dplyr::inner_join(lads_f, lads@data, by = c("id" = "label"))
+save(lads_f, file = "data/lads.RData")
+
+lsoa_f <- ggplot2::fortify(lsoa, region = "code")
+lsoa_f <- dplyr::inner_join(lsoa_f, lsoa@data, by = c("id" = "code"))
+save(lsoa_f, file = "data/lsoa.RData")
+
+rm(list = ls())
