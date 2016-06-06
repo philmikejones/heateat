cd19b1ba789a851688830f66ff60ca5907a9d47e
Phil Mike Jones <philmikejones@gmail.com>
2016-06-06 21:14:18 +0100

    move create dirs to data-raw/

--- R/create-dirs.R
+++
@@ -1,4 +0,0 @@
-# Set up the directories necessary for download.file()
-dir.create("data/shapes/lads/", recursive = TRUE, showWarnings = FALSE)
-dir.create("data/shapes/lsoa/", recursive = TRUE, showWarnings = FALSE)
-dir.create("data/raw/",         recursive = TRUE, showWarnings = FALSE)
--- data-raw/prep-raw-data.R
+++ data-raw/prep-raw-data.R
@@ -1,3 +1,9 @@
+# Set up the directories necessary for download.file()
+dir.create("data/shapes/lads/", recursive = TRUE, showWarnings = FALSE)
+dir.create("data/shapes/lsoa/", recursive = TRUE, showWarnings = FALSE)
+dir.create("data/raw/",         recursive = TRUE, showWarnings = FALSE)
+
+
 # Download necessary shapefiles from census.ukdataservice.ac.uk
 if (!file.exists("data-raw/lads.zip")) {
   download.file(
