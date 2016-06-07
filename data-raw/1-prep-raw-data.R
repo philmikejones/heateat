c1c288c9f5deb3d7ab87130053668a39f7dc5bbd
Phil Mike Jones <philmikejones@gmail.com>
2016-06-07 11:18:52 +0100

    add maptools and sp to namespace for fortify.sp()

--- data-raw/1-prep-raw-data.R
+++ data-raw/1-prep-raw-data.R
@@ -1,3 +1,8 @@
+# Require maptools and sp loaded into namespace for ggplot2::fortify.sp()
+require("maptools")
+require("sp")
+
+
 # Load LADs
 lads <- rgdal::readOGR(dsn = "extdata/lads", "england_lad_2011")
 lads@data$label <- as.character(lads@data$label)
@@ -19,13 +24,6 @@ lsoa <- sp::SpatialPolygonsDataFrame(lsoa,
   lsoa_data[row.names(lsoa_data) %in% row.names(lsoa), ])
 lsoa@data$name <- as.character(lsoa@data$name)
 
-conds <- all(grepl("Doncaster", lsoa@data$name))
-if (!conds) {
-  warning("LSOA not joined correctly (LSOA not from Doncaster present)")
-  stop()
-}
-rm(conds)
-
 
 # Load fuel poverty data
 fp <- readxl::read_excel("extdata/fuel-poverty.xlsx", sheet = "Table 3",
@@ -34,24 +32,21 @@ fp <- dplyr::select(fp, 1, 6, 7, 8)
 fp <- na.omit(fp)
 colnames(fp) <- c("code", "num_hh", "num_fph", "per_fph")
 
-if (!(nrow(fp) == nrow(lsoa_data))) {
-  warning("LSOA row numbers do not match")
-  stop()
-}
-rm(lsoa_data)
-
 
 # Join
 lsoa@data <- dplyr::inner_join(lsoa@data, fp, by = "code")
 
 
 # Fortify shapefiles
-lads_f <- fortify(lads, region = "label")
+lads_f <- ggplot2::fortify(lads, region = "label")
 lads_f <- dplyr::inner_join(lads_f, lads@data, by = c("id" = "label"))
 
-lsoa_f <- fortify(lsoa, region = "code")
+lsoa_f <- ggplot2::fortify(lsoa, region = "code")
 lsoa_f <- dplyr::inner_join(lsoa_f, lsoa@data, by = c("id" = "code"))
 
 # Export .RData files
 save(lads_f, file = "data/lads.RData")
 save(lsoa_f, file = "data/lsoa.RData")
+
+# Clean up
+rm(list = ls())
